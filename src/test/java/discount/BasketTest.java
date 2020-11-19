2bacdb673de7e067eac5e7b9b687d08c063f6189
Abhishek Giri <abhishek.giri38@gmail.com>
2020-11-19 15:13:23 +0530

    test cases added

---
+++ src/test/java/discount/BasketTest.java
@@ -0,0 +1,51 @@
+package discount;
+
+import org.junit.Before;
+import org.junit.Test;
+import retailstore.dto.Basket;
+import retailstore.dto.Item;
+import retailstore.dto.ItemType;
+import retailstore.dto.Product;
+import retailstore.dto.User;
+import retailstore.dto.UserType;
+
+import static org.junit.Assert.assertEquals;
+
+public class BasketTest {
+    private Basket basket;
+    private Item item;
+
+    @Before
+    public void setUp() {
+        User user = new User(UserType.SIMPLE, "Stark");
+        basket = new Basket(user);
+        item = new Product("something", 1000, ItemType.OTHER);
+    }
+
+    @Test
+    public void EmptyCartCostsZeroTest() {
+        assertEquals(0, basket.total(), 0.01);
+    }
+
+    @Test
+    public void SingleBasicItemCostsItsUnitPriceTest() {
+        basket.add(item);
+        assertEquals(item.getUnitPrice(), basket.total(), 0.01);
+    }
+
+    @Test
+    public void MultipleBasicItemsCostProportionallyTest() {
+        int howMany = 3;
+        basket.add(item, howMany);
+        assertEquals(howMany * item.getUnitPrice(), basket.total(), 0.01);
+    }
+
+    @Test
+    public void SeparatelyAddingTest() {
+        int howMany = 3;
+        for (int i = howMany; i > 0; i--) {
+            basket.add(item);
+        }
+        assertEquals(howMany * item.getUnitPrice(), basket.total(), 0.01);
+    }
+}
--- src/test/java/discount/DiscountTest.java
+++ src/test/java/discount/DiscountTest.java
@@ -1,4 +1,110 @@
 package discount;
 
+import org.junit.Before;
+import org.junit.Test;
+import retailstore.discount.CalculateDiscount;
+import retailstore.discount.DiscountPolicy;
+import retailstore.dto.Basket;
+import retailstore.dto.Item;
+import retailstore.dto.ItemType;
+import retailstore.dto.Product;
+import retailstore.dto.User;
+import retailstore.dto.UserType;
+
+import java.time.LocalDateTime;
+
+import static org.junit.Assert.assertEquals;
+
 public class DiscountTest {
+    private Item groceryItem;
+    private Item otherItem;
+    private User employee;
+    private User affiliate;
+    private User simpleUser;
+    private User simpleUserWith2Years;
+    private DiscountPolicy discountPolicy;
+
+    @Before
+    public void setUp() {
+        employee = new User(UserType.EMPLOYEE, "Stark");
+        affiliate = new User(UserType.AFFILIATE, "Evan");
+        simpleUser = new User(UserType.SIMPLE, "Amanda");
+        simpleUserWith2Years = new User(UserType.SIMPLE, "Lyndon", LocalDateTime.of(2014, 7, 19, 6, 40, 45));
+        groceryItem = new Product("Honey", 20, ItemType.GROCERY);
+        otherItem = new Product("PS5", 222, ItemType.OTHER);
+        discountPolicy = new CalculateDiscount();
+    }
+
+    @Test
+    public void EmployeeWithGroceryTest() {
+        Basket basket = new Basket(employee, discountPolicy);
+        basket.add(groceryItem, 4);
+        // No discount because of grocery item
+        assertEquals(80, basket.total(), 0.01);
+
+    }
+
+    @Test
+    public void EmployeeWithOtherItemTest() {
+        Basket basket = new Basket(employee, discountPolicy);
+        basket.add(otherItem, 4);
+        assertEquals(591.6, basket.total(), 0.01);
+
+    }
+
+    @Test
+    public void test_affiliateWithGrocery() {
+        Basket basket = new Basket(affiliate, discountPolicy);
+        basket.add(groceryItem, 4);
+        assertEquals(80, basket.total(), 0.01);
+
+    }
+
+    @Test
+    public void AffiliateWithOtherItemTest() {
+        Basket basket = new Basket(affiliate, discountPolicy);
+        basket.add(otherItem, 4);
+        assertEquals(764.2, basket.total(), 0.01);
+
+    }
+
+    @Test
+    public void SimpleUserWithGroceryTest() {
+        Basket basket = new Basket(simpleUser, discountPolicy);
+        basket.add(groceryItem, 4);
+        assertEquals(80, basket.total(), 0.01);
+
+    }
+
+    @Test
+    public void SimpleUserWithOtherItemTest() {
+        Basket basket = new Basket(simpleUser, discountPolicy);
+        basket.add(otherItem, 4);
+        assertEquals(848, basket.total(), 0.01);
+    }
+
+    @Test
+    public void SimpleUserWith2YearsWithGroceryTest() {
+        Basket basket = new Basket(simpleUserWith2Years, discountPolicy);
+        basket.add(groceryItem, 4);
+        assertEquals(80, basket.total(), 0.01);
+
+    }
+
+    @Test
+    public void test_simpleUserWith2YearsWithOtherItem() {
+        Basket basket = new Basket(simpleUserWith2Years, discountPolicy);
+        basket.add(otherItem, 4);
+        assertEquals(803.6, basket.total(), 0.01);
+
+    }
+
+    @Test
+    public void EmployeeWithGroceryAndOtherItemTest() {
+        Basket basket = new Basket(employee, discountPolicy);
+        basket.add(groceryItem, 4);
+        basket.add(otherItem, 4);
+        assertEquals(666.6, basket.total(), 0.01);
+
+    }
 }
